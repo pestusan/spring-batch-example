2f51bc699f0c2a5eff0260afd893ff4be4b7f203
pestusan <apester@gmx.net>
2014-02-15 18:02:28 +0100

    Example project added

---
+++ org.ape.example.spring-batch/src/main/java/org/ape/org/ape/example/spring_batch/App.java
@@ -0,0 +1,13 @@
+package org.ape.org.ape.example.spring_batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
